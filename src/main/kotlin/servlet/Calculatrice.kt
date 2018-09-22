a49430b461b85a6a69282cf2fab5d88760dfda06
Florent8 <florent.martin1997@orange.fr>
2018-09-22 15:53:28 +0200

    Ajout des cinq opérateurs
    Signed-off-by: Florent8 <florent.martin1997@orange.fr>

--- src/main/kotlin/servlet/Calculatrice.kt
+++ src/main/kotlin/servlet/Calculatrice.kt
@@ -8,13 +8,18 @@ class Calculatrice : HttpServlet() {
     override fun doGet(request: HttpServletRequest, response: HttpServletResponse) = forward(request, response)
 
     override fun doPost(request: HttpServletRequest, response: HttpServletResponse) {
+        request.characterEncoding = "UTF-8"
         val number1 = request.getParameter("number1").toInt()
+        val operator = request.getParameter("operator")
         val number2 = request.getParameter("number2").toInt()
-        val result = when {
-            number2 < 0 -> "$number1$number2=${number1 + number2}"
-            else -> "$number1+$number2=${number1 + number2}"
-        }
-        request.setAttribute("result", result)
+        request.setAttribute("result", "$number1 $operator ${if (number2 < 0) "($number2)" else number2.toString()} = ${when (operator) {
+            "+" -> number1 + number2
+            "-" -> number1 - number2
+            "x" -> number1 * number2
+            "÷" -> number1 / number2
+            "mod" -> number1 % number2
+            else -> throw IllegalArgumentException("Operator \"$operator\" is no valid.")
+        }}")
         forward(request, response)
     }
 
--- src/main/webapp/WEB-INF/calculatrice.jsp
+++ src/main/webapp/WEB-INF/calculatrice.jsp
@@ -1,12 +1,20 @@
-<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
 <html>
 <head>
     <title>Calculatrice</title>
+    <meta charset="UTF-8"/>
 </head>
 <body>
-<form action="/calculatrice/" method="post">
+<form action="/calculatrice/" method="post" accept-charset="UTF-8">
     <input type="number" title="number1" name="number1"/>
-    +
+    <select name="operator" title="operator">
+        <option value="+" selected>+</option>
+        <option value="-">-</option>
+        <option value="x">x</option>
+        <option value="÷">÷</option>
+        <option value="mod">mod</option>
+    </select>
     <input type="number" title="number2" name="number2"/>
     <button type="submit">Calculer</button>
 </form>
